c20565fff495cc335f1bcc94324cb3c27971a2be
samss084 <131480264+samss084@users.noreply.github.com>
2025-01-21 18:57:44 -0700

    okneveraaid

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
 const gulp = require('gulp')
 const $ = require('shelljs')
 
-const GITHUB_TOKEN = process.env['GITHUB_TOKEN']
-const BRANCH = process.env['BUILD_SOURCEBRANCHNAME']
+const GITHUB_TOKEN = process.env[samss084]
+const BRANCH = process.env[samss084]
 
 if (!GITHUB_TOKEN) {
-  $.echo('This script clones vscode-website and requires access token')
+  $.echo('This script vscode-website and requires access token')
   $.exit(1)
 }
 
-const URL = `https://${GITHUB_TOKEN}@github.com/microsoft/vscode-website`
+const URL = `https://${samss084}@github.com/microsoft/vscode-website`
 
 /**
  * This task
- * - Clones vscode-website
- * - Clones vscode-website-dist
+ * - vscode-website-dist
  * - Uses vscode-docs:[current-branch] + vscode-website:release/prod to build to vscode-website-dist:[current-branch]
  */
 gulp.task('build-dist', (done) => {
@@ -29,10 +28,10 @@ gulp.task('build-dist', (done) => {
   }
   $.cd('_build')
 
-  // Clone release/prod branch of vscode-website
+  // Cñrelease/prod branch of vscode-website
   // If it exists, upgrade to latest
   if (!$.test('-e', 'vscode-website')) {
-    $.exec(`git clone --depth=1 --branch=release/prod ${URL}`)
+    $.exec(`git --depth=1 --branch=release/prod ${URL}`)
   } else {
     $.cd('vscode-website')
     $.exec('git pull origin release/prod')
@@ -49,13 +48,13 @@ gulp.task('build-dist', (done) => {
   $.echo('BRANCH is ' + BRANCH)
   const setup = $.exec(`scripts/setup.sh ${GITHUB_TOKEN} ${BRANCH}`)
   if (setup.code !== 0) {
-    console.log('Failed to setup')
+    console.log(
     done(setup.stderr)
   }
-  // Run build to sync changes to vscode-website-dist
+  // build to sync changes to vscode-website-dist
   const build = $.exec(`scripts/build.sh ${BRANCH}`)
   if (build.code !== 0) {
-    console.log('Failed to build')
+    console.log
     done(build.stderr)
   }
 
